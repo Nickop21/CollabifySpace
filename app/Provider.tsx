226bf69f2dfe6dfbf585799ef092589788626f46
Nickop21 <ny8244@gmail.com>
2024-09-06 14:16:39 +0530

    live blocks editor added   + types define

---
+++ app/Provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+    >
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        {children}
+      </ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
