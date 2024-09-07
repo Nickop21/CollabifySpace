1954083bc41b89a4855cf6572abf25f414322b06
Nickop21 <ny8244@gmail.com>
2024-09-07 17:52:41 +0530

    clerk route

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,15 +5,25 @@ import {
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const  {user:clerkUsers}=useUser()
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
         return users;
+      }}
+    //  mention functionality 
+      resolveMentionSuggestions={async({text,roomId})=>{
+        const roomUsers=await getDocumentUsers({
+          roomId,
+          currentUser:clerkUsers?.emailAddresses[0].emailAddress!,
+          text})
+          return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<div>Loading…</div>}>
