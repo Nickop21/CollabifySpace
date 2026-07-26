a15062a8783c1ea55371d48c2b62df63f7218a94
Nickop21 <ny8244@gmail.com>
2026-07-27 01:09:47 +0530

    fixed css on auth pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Header = ({children,className}:HeaderProps) => {
   return (
-    <div className={cn('header shadow-sm shadow-white ',className)}>
+    <div className={cn('header shadow-sm shadow-black ',className)}>
       <Link href='/' className=''>
       <Image src="/assets/logo.svg" alt='logo' width={180} height={50} className='hidden sm:block'/>
       <Image src="/assets/logo.svg" alt='logo' width={120} height={32} className='sm:hidden'/>
